4c5539ec3b010c0f7fc2c1508b575987d2fd3243
VistoCraft <victorprezia@hotmail.com.br>
2021-06-08 11:41:04 -0300

    interação do front com o back do user

--- src/pages/RegisterUser/index.tsx
+++ src/pages/RegisterUser/index.tsx
@@ -1,25 +1,57 @@
+import { useState } from 'react';
+import api from '../../services/api';
 import Input from "../../components/Input"
 import Button from "../../components/Button"
 
 import './user.css';
 
 function RegisterUser() {
+
+  const [ email, setEmail ] = useState('');
+  const [ name, setName ] = useState('');
+  const [ type, setType ] = useState('');
+  const [ password, setPassword ] = useState('');
+  
+  async function handleSubmit() {
+
+    const data ={
+      email: email,
+      name: name,
+      type: type,
+      password: password,
+    }
+    
+    if (name !== '' && email !== '' && password !== '' && type !== '') {
+      const response = await api.post('/user', data)
+      
+      if (response.status === 200) {
+        /* window.location.href = '' */
+        alert("Cadastrado com sucesso");
+      }else{
+        alert("Não Cadastrado");        
+      }
+    }else{
+
+    }
+    
+  }
+
   return (
     <div className="content">
       <div className="content-form">
         <h1 className="title">Cadastro</h1>
         <div className="form">
-          <form action="" >
+          <form onSubmit={handleSubmit}>
             <div className="input-text">
-              <Input type="text" name="name" placeholder="Nome do Usuario" />
-              <Input type="email" name="email" placeholder="Email" />
-              <Input type="password" name="senha" placeholder="Senha" />
+              <Input type="text" name="name" placeholder="Nome do Usuario" value ={name} onChange = { event => setName(event.target.value)}/>
+              <Input type="email" name="email" placeholder="Email" value ={email} onChange = { event => setEmail(event.target.value)}/>
+              <Input type="password" name="senha" placeholder="Senha" value ={password} onChange = { event => setPassword(event.target.value)}/>
             </div>
             <label htmlFor="">Status: </label>
             <div className="input-radio">
-              <Input type="radio" name="status" value="1" checked />
-              <label className="option-radio" htmlFor="">Aluno</label>
-              <Input type="radio" name="status" value="0" />
+              <Input type="radio" name="status" value="1" checked = {status === "1"} onChange = { event => setType(event.target.value)} />
+              <label className="option-radio" htmlFor="">Aluno</label>              
+              <Input type="radio" name="status" value="0" checked = {status === "0"} onChange = { event => setType(event.target.value)} />
               <label htmlFor="">Professor</label>
             </div>
             <Button type="submit">Criar</Button>
