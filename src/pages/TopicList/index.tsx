1bf9658e5d177f998c4ce32f3276e5470d0190a7
MateusNazare <mateus.nazare5@gmail.com>
2021-06-22 16:21:04 -0300

    terminando list topics

--- src/pages/TopicList/index.tsx
+++ src/pages/TopicList/index.tsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import api from '../../services/api';
 
 import './styles.css';
 
+interface Topic {
+  id: string;
+  name: string;
+  description: string;
+}
+
 
 function TopicList() {
 
+  const [topics, setTopic] = useState<Topic[]>([])
+
+  useEffect(() => {
+    api.get('/topics').then(response => {
+      setTopic(response.data);
+    });
+  }, []);
+
+
   return (
+
     <div className="body">
       <div className="content">
-        <div className="content-form">
-          <h1 className="title">Atividade</h1>
-          <h2 className="title">Professor</h2>
-          <h2 className="title">Matéria</h2>
-          <Button>Acessar</Button>
-        </div>
+        {topics.map((topic: Topic) => {
+          return (
+            <div className="content-form">
+              <h1 className="title">{topic.name}</h1>
+              <h2 className="title">{topic.description}</h2>
+              <Button>Acessar</Button>
+            </div>
+          )
+        })}
+
       </div>
     </div>
   );
